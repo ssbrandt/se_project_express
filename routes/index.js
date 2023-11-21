4318de757583239d2f501b022f9e063e5e72f827
ssbrandt <sierrasbrandt@gmail.com>
2023-11-21 13:07:46 -0500

    minor reviewer fix

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const clothingItem = require("./clothingItems");
 const user = require("./users");
-const errors = require("../utils/errors");
+const { errors } = require("../utils/errors");
 
 router.use("/items", clothingItem);
 router.use("/users", user);

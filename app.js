249dbf671729b78a9e01badfbb159b0e207eca8d
ssbrandt <sierrasbrandt@gmail.com>
2024-01-29 15:45:11 -0500

    add crash test for review

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ app.use(cors());
 app.use(express.json());
 const routes = require("./routes");
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Server will crash now");
+  }, 0);
+});
+
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
